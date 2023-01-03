21665027e0feae599525feb8d30ca3c880982840
raihan2bd <raihan2bd.official@gmail.com>
2023-01-03 18:47:50 +0600

    Select a movie api and test it

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import './style.css';
+
+// external api url
+const MOVIE_API = 'https://api.tvmaze.com/seasons/1/episodes';
+
+const fetchdata = async () => {
+  const res = await fetch(MOVIE_API);
+  const result = await res.json();
+  console.log(result);
+};
+
+window.onload = () => {
+  fetchdata();
+};
