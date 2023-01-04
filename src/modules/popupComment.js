3bf5a002193d7289edbd10f60e7f1affa6763017
Salwa99 <s.ritage99@gmail.com>
2023-01-04 17:04:03 +0100

    Fix linters errors

--- src/modules/popupComment.js
+++ src/modules/popupComment.js
@@ -1,40 +1,39 @@
-import { modalContainer } from "./domSelector.js";
+import { modalContainer } from './domSelector.js';
 
 const render = (data) => {
   // modal card
-  const modalCard = document.createElement("div");
-  modalCard.className = "modal-card";
+  const modalCard = document.createElement('div');
+  modalCard.className = 'modal-card';
 
   // button
-  const btnCross = document.createElement("button");
-  btnCross.className = "btn-cross";
-  btnCross.innerHTML =
-    '<img src="./assets/img/icons8-multiply-24.png" alt="X" />';
-  btnCross.addEventListener("click", () => {
-    modalContainer.classList.remove("show");
+  const btnCross = document.createElement('button');
+  btnCross.className = 'btn-cross';
+  btnCross.innerHTML = '<img src="./assets/img/icons8-multiply-24.png" alt="X" />';
+  btnCross.addEventListener('click', () => {
+    modalContainer.classList.remove('show');
   });
 
   // card img
-  const cardImg = document.createElement("div");
-  cardImg.className = "card-img";
+  const cardImg = document.createElement('div');
+  cardImg.className = 'card-img';
   cardImg.innerHTML = `<img
   src="${data.image.original}"
   alt="${data.name}"
 />`;
 
   // card title
-  const title = document.createElement("h2");
-  title.className = "card-title";
+  const title = document.createElement('h2');
+  title.className = 'card-title';
   title.innerText = data.name;
 
   // card des
-  const des = document.createElement("div");
-  des.className = "card-des";
+  const des = document.createElement('div');
+  des.className = 'card-des';
   des.innerHTML = data.summary;
 
   // card spec group
-  const cardSpec = document.createElement("div");
-  cardSpec.className = "card-spec-group";
+  const cardSpec = document.createElement('div');
+  cardSpec.className = 'card-spec-group';
   cardSpec.innerHTML = `<ul class="card-spec">
   <li class="spec-item">Season: ${data.season}</li>
   <li class="spec-item">Duration: ${data.runtime}</li>
@@ -44,17 +43,17 @@ const render = (data) => {
   <li class="spec-item">Rating: ${data.rating.average}</li>
 </ul>`;
 
-  const commentSection = document.createElement("div");
-  commentSection.className = "comment-section";
+  const commentSection = document.createElement('div');
+  commentSection.className = 'comment-section';
 
-  const commentHeader = document.createElement("h3");
-  commentHeader.className = "comment-title";
+  const commentHeader = document.createElement('h3');
+  commentHeader.className = 'comment-title';
   commentHeader.innerText = `Comments (${data.comments.length})`;
 
-  const commentGroup = document.createElement("ul");
-  commentGroup.className = "comment-goup";
+  const commentGroup = document.createElement('ul');
+  commentGroup.className = 'comment-goup';
 
-  let commentItems = "";
+  let commentItems = '';
 
   if (data.comments.length > 0) {
     data.comments.forEach((item) => {
@@ -67,21 +66,22 @@ const render = (data) => {
 
   commentSection.append(commentHeader, commentGroup);
 
-  modalCard.append(btnCross, cardImg, title, des, cardSpec, commentSection); // append child elements in cardModal
+  // append child elements in cardModal
+  modalCard.append(btnCross, cardImg, title, des, cardSpec, commentSection);
 
-  modalContainer.innerHTML = "";
+  modalContainer.innerHTML = '';
   modalContainer.append(modalCard); // append cardModal
 };
 
 const fetchSingleShowComment = async (e) => {
-  modalContainer.classList.add("show");
+  modalContainer.classList.add('show');
   const { id } = e.target.parentElement.parentElement;
   const url = `https://api.tvmaze.com/episodes/${id}`;
   const res = await fetch(url);
   const result = await res.json();
 
   const commentRes = await fetch(
-    `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/Y1Ocl2k5LoJdVEhHia5O/comments?item_id=${id}`
+    `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/Y1Ocl2k5LoJdVEhHia5O/comments?item_id=${id}`,
   );
 
   let filterResult = { ...result };
