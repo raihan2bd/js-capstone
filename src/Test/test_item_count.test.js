eee850783de48129980dbe2f39a79137ad0843e0
raihan2bd <raihan2bd.official@gmail.com>
2023-01-05 18:28:10 +0600

    Fix the linters errors

--- src/Test/test_item_count.test.js
+++ src/Test/test_item_count.test.js
@@ -1,9 +1,9 @@
-import itemCounter from "../modules/itemCounter.js";
+import itemCounter from '../modules/itemCounter.js';
 /**
  * @jest-environment jsdom
  */
 
-describe("Test Display Item counter", () => {
+describe('Test Display Item counter', () => {
   document.body.innerHTML = `
     <h1 class='title'>Favorite Tv Shows <span id='item_count'>(0)</span></h1>
     <ul class='items'>
@@ -14,17 +14,17 @@ describe("Test Display Item counter", () => {
       <li class='item'>item 5</li>
     </ul>`;
 
-  test("Item count should be 5", () => {
-    const itemContainer = document.querySelector(".items");
-    const itemCountContainer = document.getElementById("item_count");
+  test('Item count should be 5', () => {
+    const itemContainer = document.querySelector('.items');
+    const itemCountContainer = document.getElementById('item_count');
 
     const count = itemCounter(itemCountContainer, itemContainer);
     expect(count).toBe(5);
   });
 
-  test("Item count innterText should be equal to item container chlild count", () => {
-    const itemContainer = document.querySelector(".items");
-    const itemCountContainer = document.getElementById("item_count");
+  test('Item count innterText should be equal to item container chlild count', () => {
+    const itemContainer = document.querySelector('.items');
+    const itemCountContainer = document.getElementById('item_count');
 
     const count = itemCounter(itemCountContainer, itemContainer);
     expect(itemContainer.childElementCount).toBe(count);
