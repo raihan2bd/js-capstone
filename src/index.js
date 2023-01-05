b01c21fc71f67f1813de3e80adaaa2283b730fe7
Salwa99 <s.ritage99@gmail.com>
2023-01-05 10:49:00 +0100

    Fix linters errors

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 // import static files
-import "./style.css";
-import "./assets/img/icons8-favorite-30.png";
-import "./assets/img/icons8-menu-rounded-24.png";
-import "./assets/img/icons8-multiply-24.png";
+import './style.css';
+import './assets/img/icons8-favorite-30.png';
+import './assets/img/icons8-menu-rounded-24.png';
+import './assets/img/icons8-multiply-24.png';
 
-import fetchTvShow from "./modules/displayItems.js";
+import fetchTvShow from './modules/displayItems.js';
 
 // load the item list on the fly.
 window.onload = () => {
--- src/modules/apiUrls.js
+++ src/modules/apiUrls.js
@@ -1,2 +1,2 @@
-export const BASE_URL = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/Y1Ocl2k5LoJdVEhHia5O`;
-export const MOVIE_API = `https://api.tvmaze.com/seasons/1/episodes`;
+export const BASE_URL = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/Y1Ocl2k5LoJdVEhHia5O';
+export const MOVIE_API = 'https://api.tvmaze.com/seasons/1/episodes';
