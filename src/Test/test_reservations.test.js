801fd6552b1e862e23a454ac5a9d4c41721d74bb
lugard1 <agulugard@gmail.com>
2023-01-05 15:59:45 +0100

    test added for reservation counter

---
+++ src/Test/test_reservations.test.js
@@ -0,0 +1,41 @@
+import reservationCounter from "../modules/reservationCounter.js";
+
+describe("Test Display Reservation counter", () => {
+  document.body.innerHTML = `
+    <h3 class='reservation-title'>reservations <span id='reservation_count'>(0)</span></h3>
+    <ul class='reservation-item-group'>
+      <li class='reservation-item'>Reservation 1</li>
+      <li class='reservation-item'>Reservation 2</li>
+      <li class='reservation-item'>Reservation 3</li>
+      <li class='reservation-item'>Reservation 4</li>
+      <li class='reservation-item'>Reservation 5</li>
+    </ul>`;
+
+  test("Reservation count should be 5", () => {
+    const reservationContainer = document.querySelector(
+      ".reservation-item-group"
+    );
+    const reservationCountContainer =
+      document.getElementById("reservation_count");
+
+    const count = reservationCounter(
+      reservationCountContainer,
+      reservationContainer
+    );
+    expect(count).toBe(5);
+  });
+
+  test("Reservation count innerText should be equal to reservation container chlild count", () => {
+    const reservationContainer = document.querySelector(
+      ".reservation-item-group"
+    );
+    const reservationCountContainer =
+      document.getElementById("reservation_count");
+
+    const count = reservationCounter(
+      reservationCountContainer,
+      reservationContainer
+    );
+    expect(reservationContainer.childElementCount).toBe(count);
+  });
+});
