1a0c2aeb21f03e92fbd30ac7ff554a169d94510e
lugard1 <agulugard@gmail.com>
2023-01-05 11:41:39 +0100

    add newReservation-section

--- src/modules/popupReservation.js
+++ src/modules/popupReservation.js
@@ -1,6 +1,52 @@
 import { BASE_URL } from './apiUrls.js';
 import { modalContainer } from './domSelector.js';
 
+const createNewReservation = async (url, data, reservationContainer, Form) => {
+  const res = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  });
+
+  if (!res.ok && res.status !== 201) {
+    return;
+  }
+
+  const fetchComUrl = `${BASE_URL}/reservations?item_id=${data.item_id}`;
+
+  const resp = await fetch(fetchComUrl);
+
+  const result = await resp.json();
+
+  // manupulate the dom
+  const reservationHeader = document.createElement('h3');
+  reservationHeader.className = 'reservation-title';
+  reservationHeader.innerText = `Reservations (${result.length})`;
+
+  const reservationGroup = document.createElement('ul');
+  reservationGroup.className = 'reservation-group';
+
+  let reservationItems = '';
+
+  if (result.length > 0) {
+    result.forEach((item) => {
+      reservationItems += `<li class='reservation-item'>
+      ${item.date_start} - ${item.date_end} by ${item.username}</li>`;
+    });
+  }
+
+  reservationGroup.innerHTML = reservationItems; // append reservation list
+  reservationContainer.innerHTML = '';
+
+  reservationContainer.append(reservationHeader, reservationGroup);
+
+  Form.elements.name.value = '';
+  Form.elements.start_date.value = '';
+  Form.elements.end_date.value = '';
+};
+
 const render = (data) => {
   // modal card
   const modalCard = document.createElement('div');
@@ -67,9 +113,49 @@ const render = (data) => {
 
   reservationSection.append(reservationHeader, reservationGroup);
 
+  // Add comment
+  const reservationForm = document.createElement('div');
+  reservationForm.className = 'reservation-form';
+  const addReservation = document.createElement('h3');
+  addReservation.className = 'reservation-title';
+  addReservation.innerText = 'Add a Reservation';
+  const Form = document.createElement('form');
+  Form.innerHTML = `
+  <input type="text"  name="name" id="name" placeholder="Your name" maxlength="10" required>
+  <label for='start_date'>Add your start date below</label>
+  <input type="date"  name="start_date" id="start_date" placeholder="Add your start date here." required>
+  <label for='end_date'>Add your end date below</label>
+  <input type="date"  name="end_date" id="end_date" placeholder="Add your end date here." required>
+  <button type="submit" id="submitBtn">Reserve</button>`;
+
+  reservationForm.append(addReservation, Form);
+
+  Form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const name = Form.elements.name.value;
+    const startDate = Form.elements.start_date.value;
+    const endDate = Form.elements.end_date.value;
+    const reserveData = {
+      item_id: data.id,
+      username: name,
+      date_start: startDate,
+      date_end: endDate,
+    };
+    const url = `${BASE_URL}/reservations/`;
+    createNewReservation(url, reserveData, reservationSection, Form);
+  });
+
   // append child elements in cardModal
   // eslint-disable-next-line max-len
-  modalCard.append(btnCross, cardImg, title, des, cardSpec, reservationSection);
+  modalCard.append(
+    btnCross,
+    cardImg,
+    title,
+    des,
+    cardSpec,
+    reservationSection,
+    reservationForm,
+  );
 
   modalContainer.innerHTML = '';
   modalContainer.append(modalCard); // append cardModal
